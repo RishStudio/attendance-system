f8f08639db3f5d67dd3caf28c85866fa2f5a0176
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-01-31 19:21:12 +0530

    rgr

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,13 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BOB attendance system",
+  title: "BOP Attendance System",
   description: "Rishmika Idea Fixed by Vimukthi Indunil",
+  keywords: "attendance, system, BOP, Rishmika, Vimukthi, tracking, management",
+  author: "Rishmika and Vimukthi Indunil",
+  viewport: "width=device-width, initial-scale=1.0",
+  charset: "UTF-8",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -19,6 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Rishmika and Vimukthi Indunil" />
+        <meta name="keywords" content="attendance, system, BOP, Rishmika, Vimukthi, tracking, management" />
+        <meta name="description" content="Idea From Rishmika Sandanu & Bug Fixed by Vimukthi Indunil" />
+        <link rel="icon" href="https://cdn.imrishmika.site/images/attendance.png" />
+        <title>BOP Attendance System</title>
+      </head>
       <body
         className={`${inter.className} bg-background text-text flex flex-col min-h-screen`}
       >
