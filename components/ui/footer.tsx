043f302853940ced27e7487a19e59040c81f3b65
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-02-05 16:46:49 +0530

    erg

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="w-full border-t bg-gray-900">
       <div className="container mx-auto px-4 py-2 flex flex-col sm:flex-row items-center justify-between">
         <p className="text-sm text-gray-300 text-center sm:text-left">
-          © {new Date().getFullYear()} <a href="https://imrishmika.site" className="text-blue-400 hover:underline">imrishmika.site</a>. All rights reserved. This is Made For Prefect Board of MRCM.
+          © {new Date().getFullYear()} <a href="https://imrishmika.site" className="text-blue-400 hover:underline">Rishmika Sandanu</a>. All rights reserved. This is Made For Prefect Board of MRCM.
         </p>
       </div>
     </footer>
