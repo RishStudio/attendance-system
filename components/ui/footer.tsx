b20043aaf506704e9142f3c66596c2883dc119ee
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-02-05 16:42:35 +0530

    th

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
-    <footer className="w-full border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <footer className="w-full border-t bg-neutral-100">
       <div className="container flex h-14 items-center">
         <div className="flex flex-1 items-center justify-between">
-          <p className="text-sm text-muted-foreground">
+          <p className="text-sm text-gray-600">
             © {new Date().getFullYear()} Rishmika Sandanu. All rights reserved. This is Made For Prefect Board of MRCM
           </p>
         </div>
