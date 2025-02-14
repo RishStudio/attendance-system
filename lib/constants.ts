4eb904002ad7be34759a3e8a7a9c782bd2bc45ab
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-02-14 16:58:40 +0530

    yj

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+import { PrefectRole } from './types';
+
+export const roles: PrefectRole[] = [
+  'Head',
+  'Deputy',
+  'Senior Executive',
+  'Executive',
+  'Super Senior',
+  'Senior',
+  'Junior',
+  'Sub',
+  'Apprentice'
+];
