5a32c4d3d96719e4f2547a8c49e14bb454cf51cc
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-04-03 15:45:15 +0530

    Update footer.tsx

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="w-full border-t bg-black">
       <div className="container mx-auto px-4 py-4 flex flex-col items-center">
         <p className="text-sm text-gray-400 text-center">
-          © {new Date().getFullYear()} <a href="https://imrishmika.site" className="text-blue-400 hover:underline">Rishmika Sandanu</a>. All rights reserved. This is Made For Prefect Board of MRCM.
+          © {new Date().getFullYear()} <a href="https://rylix.imrishmika.site" className="text-white hover:no-underline">Rylix Solution</a> & <a href="https://imrishmika.site" className="text-white hover:no-underline">ImRishmika</a>. All rights reserved. This is Made For Prefect Board of MRCM.
         </p>
       </div>
     </footer>
