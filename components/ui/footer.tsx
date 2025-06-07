9d12ade1fc5a6f411f8e8cca11bd914824d46399
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2025-06-07 19:58:55 +0530

    Footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,9 +1,23 @@
 export function Footer() {
   return (
-    <footer className="w-full border-t bg-black">
+    <footer className="w-full border-t border-gray-200 bg-white bg-opacity-10 backdrop-blur-lg">
       <div className="container mx-auto px-4 py-4 flex flex-col items-center">
-        <p className="text-sm text-gray-400 text-center">
-          © {new Date().getFullYear()} <a href="https://rylix.imrishmika.site" className="text-white hover:no-underline">Rylix Solution</a> & <a href="https://imrishmika.site" className="text-white hover:no-underline">ImRishmika</a>. All rights reserved. This is Made For Prefect Board of MRCM.
+        <p className="text-sm text-gray-300 text-center">
+          © {new Date().getFullYear()}{" "}
+          <a
+            href="https://rylix.imrishmika.site"
+            className="text-white hover:underline"
+          >
+            Rylix Solution
+          </a>{" "}
+          &amp;{" "}
+          <a
+            href="https://imrishmika.site"
+            className="text-white hover:underline"
+          >
+            ImRishmika
+          </a>
+          . All rights reserved. Made for the Prefect Board of MRCM.
         </p>
       </div>
     </footer>
